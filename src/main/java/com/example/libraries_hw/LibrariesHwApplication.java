1268331a8823cbbf9d0afb71e278f496b7d8e394
Dima <akmeev2011@yandex.ru>
2022-11-16 20:17:50 +0300

    Initial commit

---
+++ src/main/java/com/example/libraries_hw/LibrariesHwApplication.java
@@ -0,0 +1,13 @@
+package com.example.libraries_hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrariesHwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibrariesHwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/libraries_hw/LibrariesHwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libraries_hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrariesHwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
